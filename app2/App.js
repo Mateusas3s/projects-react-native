f7082b2094c4315a9cf8fa60aecf1f8c8ad94d91
Mateus <mateus@mateus.meuintelbras.local>
2019-01-27 18:02:53 -0200

    creating new styles

--- app2/App.js
+++ app2/App.js
@@ -1,35 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const styles = StyleSheet.create({
-  styleView: {
+  main: {
     flex: 1,
-    backgroundColor: '#d37',
+    backgroundColor: '#222',
   },
-  styleText: {
-    fontSize: 30,
+  top: {
+    flex: 2,
+    fontSize: 20,
     fontFamily: 'Roboto',
     fontWeight: 'bold',
     fontStyle: 'italic',
     textAlign: 'center',
-    textAlignVertical: 'center',
     color: '#fff',
     backgroundColor: '#655',
-    marginTop: 60,
-    marginHorizontal: 50,
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOffset: {width:1, height:5},
-    shadowOpacity: 0.75,
-    shadowRadius: 5,
+    marginTop: 30,
+    paddingTop: 5,
+  },
+  cont: {
+    flex: 15,
+    fontSize: 10,
+    fontWeight: '100',
+    textAlign: 'left',
+    color: '#000',
+    backgroundColor: '#234',
+  },
+  button: {
+    marginBottom: 50,
+    marginHorizontal: 20,
+  },
+  rod: {
+    flex: 1,
+    fontSize: 10,
+    fontWeight: '100',
+    textAlign: 'left',
+    color: '#245',
+    backgroundColor: '#f29',
   }
 });
 
+const bottonPress = () =>{
+  alert("Botão Pressionado!");
+}
+
 export default class App extends React.Component {
   render() {
+    const {main, top, cont, button, rod} = styles;
     return (
-      <View style={styles.styleView}>
-        <Text style={styles.styleText}>Frases do Dia</Text>
+
+      <View style={main}>
+        <Text style={top}>Frases do Dia</Text>
+        <Text style={cont}>Frases do Dia</Text>
+        <Button 
+          style={button}
+          title="Clique aqui"
+          color="#837493"
+          accessibilityLabel="Clique para abrir as notícias!"
+          onPress={bottonPress}
+        />
+        <Text style={rod}>Frases do Dia</Text>
       </View>
     );
   }
