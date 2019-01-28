b0b2d5697270ae6964caded096b23d69fe5a61b2
Mateus <mateus@mateus.meuintelbras.local>
2019-01-28 00:10:21 -0200

    finishing app Frase do Dia

--- app2/App.js
+++ app2/App.js
@@ -1,77 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert} from 'react-native';
 
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    backgroundColor: '#222',
-  },
-  top: {
-    flex: 2,
-    fontSize: 20,
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
-    fontStyle: 'italic',
-    textAlign: 'center',
-    color: '#fff',
-    backgroundColor: '#655',
-    marginTop: 30,
-    paddingTop: 5,
-  },
-  cont: {
-    flex: 5,
-    fontSize: 10,
-    fontWeight: '100',
-    textAlign: 'left',
-    color: '#000',
-    backgroundColor: '#7a6',
-  },
-  image:{
-    flex:10
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   button: {
-    flex:1,
-    marginBottom: 50,
-    marginHorizontal: 20,
+    backgroundColor: '#538530',
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    marginTop: 20,
+    borderRadius: 2,
+    elevation: 2, 
   },
-  rod: {
-    flex: 1,
-    fontSize: 10,
-    fontWeight: '100',
-    textAlign: 'left',
-    color: '#245',
-    backgroundColor: '#f29',
+  textButton: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    
   }
 });
 
-const bottonPress = () =>{
-  alert("Botão Pressionado!");
+const createNewPhrase = () =>{
+  var num_rand = Math.random();
+  num_rand = Math.floor(num_rand*5);
+
+  var phrases = Array();
+  phrases[0] = 'Faça. Ou não faça. Não existe a tentativa.';
+  phrases[1] = 'O medo é o caminho para o lado negro.';
+  phrases[2] = 'Sempre passar o que você aprendeu.';
+  phrases[3] = 'Seres luminosos somos nós. Não esta matéria bruta.';
+  phrases[4] = 'Que a Força esteja com você.';
+
+  var choiced_phrase = phrases[num_rand];
+  Alert.alert(choiced_phrase);
 }
 
 export default class App extends React.Component {
   render() {
-    const {main, top, cont, image, button, rod} = styles;
+    const {main, button, textButton} = styles;
     return (
 
       <View style={main}>
-        <Text style={top}>Frases do Dia</Text>
-        <Text style={cont}>Frases do Dia</Text>
-        <Button 
-          style={button}
-          title="Clique aqui"
-          color="#837493"
-          accessibilityLabel="Clique para abrir as notícias!"
-          onPress={bottonPress}
-        />
-        <Image 
-          style={image}
-          source={require('./assets/imgs/uvas.png')}
-        />
-        <Image 
-          style={image}
-          source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
-        />
-        <Text style={rod}>Frases do Dia</Text>
+        <Image source={require('./assets/imgs/logo.png')}/>
+        <TouchableOpacity onPress={createNewPhrase} style={button}>
+          <Text style={textButton}>
+            Nova Frase
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
