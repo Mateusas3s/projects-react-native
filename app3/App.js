191dd9cac50a60e7bd5a47d1bfab622c93c79c43
Mateus <mateus@mateus.meuintelbras.local>
2019-01-29 11:27:35 -0200

    creating custom components

--- app3/App.js
+++ app3/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+class MyComponent extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>{this.props.prop1}</Text>
+        <Text>{this.props.prop2}</Text>
+        <Text>{this.props.prop3}</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+  render() {
+    return (
+      <MyComponent prop1='Banana' prop2='Abacaxi' prop3='Uva'></MyComponent>
+    );
+  }
+}
