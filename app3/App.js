dba5fc7d7509b59dec258cc9483b9842fef2e5b3
Mateus <mateus@mateus.meuintelbras.local>
2019-01-29 14:13:28 -0200

    creating logic game part1

--- app3/App.js
+++ app3/App.js
@@ -1,22 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-class MyComponent extends React.Component{
-  render(){
-    return(
-      <View>
-        <Text>{this.props.prop1}</Text>
-        <Text>{this.props.prop2}</Text>
-        <Text>{this.props.prop3}</Text>
-      </View>
-    );
-  }
-}
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {user_choice: ''}
+  }
+
+  jokenpo(user_choice){
+    this.setState({user_choice: user_choice});
+  }
+
   render() {
     return (
-      <MyComponent prop1='Banana' prop2='Abacaxi' prop3='Uva'></MyComponent>
+      <View>
+        <Text>
+          Escolha do Computador
+        </Text>
+        <Text>
+          Escolha do Usuário: {this.state.user_choice}
+        </Text>
+        <Text>
+          Resultado
+        </Text>
+        <Button
+          title='Pedra'
+          onPress={() => {this.jokenpo('Pedra')}}
+        />
+        <Button
+          title='Papel'
+          onPress={() => {this.jokenpo('Papel')}}
+        />
+        <Button
+          title='Tesoura'
+          onPress={() => {this.jokenpo('Tesoura')}}
+        />
+      </View>
     );
   }
 }
