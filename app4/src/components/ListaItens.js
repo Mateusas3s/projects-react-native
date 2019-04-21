b586ce27071a391e3a4e051d7d111d4e97c7bd89
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2019-04-21 17:12:56 -0300

    criando requisições HTTP e seus tratamentos

--- app4/src/components/ListaItens.js
+++ app4/src/components/ListaItens.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 import Itens from './Itens';
+import axios from 'axios';
 
 export default class ListaItens extends React.Component {
+
+  componentWillMount(){
+    //request Http
+    axios.get('faus.com.br/recursos/c/dmairr/api/Itens.html')
+      .then(response => {console.log(response);})
+      .catch(() => {console.log('Erro ao recuperar os dados!')});
+  }
   
   render() {
     return (
