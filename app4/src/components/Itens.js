ed433510bb7d7857c634a6e92359a9d9d45f9f5e
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2019-04-21 16:41:40 -0300

    criando estrutura de componentes

---
+++ app4/src/components/Itens.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default class Itens extends React.Component {
+  
+  render() {
+    return (
+      <Text>Detalhes do Item</Text>
+    );
+  }
+}
--- app4/src/components/ListaItens.js
+++ app4/src/components/ListaItens.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
+import Itens from './Itens';
 
 export default class ListaItens extends React.Component {
-
+  
   render() {
     return (
       <View>
