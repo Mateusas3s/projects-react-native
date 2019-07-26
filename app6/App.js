e053047b3cbfa21e81481d561f226002fc27720d
mateusas3s <mateusaugusto-2009@hotmail.com>
2019-07-26 14:48:57 -0300

    iniciando projeto 6

--- app6/App.js
+++ app6/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const logo = require('./src/imgs/logo.png');
+const btnJogar = require('./src/imgs/botao_jogar.png');
+const btnSobreJogo = require('./src/imgs/sobre_jogo.png');
+const btnOutrosJogos = require('./src/imgs/outros_jogos.png');
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Image source={logo} />
+        <Image source={btnJogar} />
+      </View>
+
+      <View>
+        <Image source={btnSobreJogo} />
+        <Image source={btnOutrosJogos} />
+      </View>
     </View>
   );
 }
