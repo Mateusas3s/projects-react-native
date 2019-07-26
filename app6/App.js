702e88b9db39c0089d6cb752f57c02852a9dc2c6
mateusas3s <mateusaugusto-2009@hotmail.com>
2019-07-26 19:13:34 -0300

    finalizando app6

--- app6/App.js
+++ app6/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Router, Scene } from 'react-native-router-flux';
-
-import Principal from './src/components/Principal';
-import SobreJogo from './src/components/SobreJogo';
-import OutrosJogos from './src/components/OutrosJogos';
+import Rotas from './src/Rotas';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Router sceneStyle={{ paddingTop: 50 }}>
-        <Scene key='principal' component={Principal} initil title="Cara ou coroa" />
-        <Scene key='sobrejogo' component={SobreJogo} title="Sobre o Jogo" />
-        <Scene key='outrosjogos' component={OutrosJogos} title="Outros Jogos" />
-      </Router>
+      <Rotas />
     );
   }
 }
